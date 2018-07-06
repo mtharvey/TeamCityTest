7e3060e07b8c79ab95a1f6c7ac7d7abbdbd107db
Mike Harvey <mharvey@async7.com>
2018-07-06 11:39:20 -0500

    Adding unit test project

---
+++ TeamCitySimpleTests/MessageMakerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamCitySimple;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCitySimple.Tests
+{
+    [TestClass()]
+    public class MessageMakerTests
+    {
+        [TestMethod()]
+        public void GetMessageTest()
+        {
+            var message = MessageMaker.GetMessage();
+            Assert.AreEqual(false, string.IsNullOrWhiteSpace(message));
+        }
+
+    }
+}
