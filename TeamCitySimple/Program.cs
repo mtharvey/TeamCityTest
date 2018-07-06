323a81095e5851c9d9b106be9820f31a40b98f4e
Mike Harvey <mharvey@async7.com>
2018-07-06 09:38:09 -0500

    Adding a test project to repo

---
+++ TeamCitySimple/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCitySimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shibby");
+            System.Threading.Thread.Sleep(1500);
+        }
+    }
+}
