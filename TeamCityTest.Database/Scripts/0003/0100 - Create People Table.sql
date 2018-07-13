b05343697863606398081f95569ee02b8d9158b8
Mike Harvey <mharvey@async7.com>
2018-07-13 11:58:37 -0500

    Added new table

---
+++ TeamCityTest.Database/Scripts/0003/0100 - Create People Table.sql
@@ -0,0 +1,16 @@
+﻿
+
+/****** Object:  Table [dbo].[People]    Script Date: 7/13/2018 11:57:08 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[People](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL
+) ON [PRIMARY]
+
+GO
